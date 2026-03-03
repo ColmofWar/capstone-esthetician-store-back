6941d4080d7d5af5671d1c9eba07b6486c3823b4
ColmofWar <joshuaallendimarco@gmail.com>
2026-03-02 20:53:02 -0500

    updated tests that broke from recent changes

--- sql/esthetician-store.sql
+++ sql/esthetician-store.sql
@@ -7,7 +7,7 @@ CREATE DATABASE esthetician_store;
 
 \i esthetician-store-schema.sql
 \i esthetician-store-seed.pg.sql
-
+/*
 \echo 'Delete and recreate esthetician_store_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
@@ -16,3 +16,4 @@ CREATE DATABASE esthetician_store_test;
 \connect esthetician_store_test
 
 \i esthetician-store-schema.sql
+*/
